88f5818d093052066a6404a608f43e4ba33b3747
Suvash Thapaliya <suvash@gmail.com>
2012-03-22 20:20:32 +0100

    Adds routes

--- serve.py
+++ serve.py
@@ -1,10 +1,18 @@
 import os
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
